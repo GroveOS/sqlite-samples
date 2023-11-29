f6f9aedc9d48a21dda185a7e0f40fdacca8dd7d2
Matthew Larkin <info@groveos.com>
2023-11-28 22:38:09 -0500

    Update finance.sql

--- finance.sql
+++ finance.sql
@@ -5,7 +5,7 @@ CREATE TABLE "invoices" (
     "due_date" TEXT NOT NULL CHECK("due_date" LIKE '____-__-__'),
     "amount" REAL NOT NULL CHECK("amount" >= 0),
     "canceled" INTEGER DEFAULT 0 CHECK("cancelled" IN (0, 1)),
-    FOREIGN KEY("person_id") REFERENCES "people"("id") ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY("person_id") REFERENCES "people"("id") ON UPDATE CASCADE ON DELETE CASCADE
 ) STRICT;
 
 CREATE TABLE "payments" (
@@ -14,5 +14,5 @@ CREATE TABLE "payments" (
     "date" TEXT NOT NULL CHECK("date" LIKE '____-__-__'),
     "amount" REAL NOT NULL CHECK("amount" >= 0),
     "method" TEXT NOT NULL CHECK("method" IN ('cash', 'card', 'bank_transfer', 'check', 'other')),
-    FOREIGN KEY("invoice_id") REFERENCES "invoices"("invoice_id") ON UPDATE CASCADE ON DELETE CASCADE
+    FOREIGN KEY("invoice_id") REFERENCES "invoices"("id") ON UPDATE CASCADE ON DELETE CASCADE
 ) STRICT;
